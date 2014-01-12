71ad8aeb35633f1df97b37a97596b304a4c93967
kwarpechowski <kwarpechowski@hakama.pl>
2014-01-12 16:43:58 +0100

    clear js

--- sim.js
+++ sim.js
@@ -3,15 +3,9 @@ var ctx = canvas.getContext('2d');
 
 var N = 2,
     size = 500,
-    x = [],
-    y = [],
-    cx = [],
-    cy = [],
-    c = [],
-    pr = [],
-    hist = [],
     v1 = [],
     v2 = [],
+    objects = [],
     show = false,
     line = false,
     x1,x2,y1,y2,t,n,u,l;
@@ -34,54 +28,59 @@ function randomPos (p) {
 
 function init() {
     for(var i = 0; i<N; i++) {
-        pr[i] = randomFromInterval(10, 40);
-        x[i] = randomPos(pr[i]);
-        y[i] = randomPos(pr[i]);
-        hist[i] = [];
-        c[i] = color();
+        var p = randomFromInterval(10, 40);
+        objects[i] = {
+           x: randomPos(p),
+           y: randomPos(p),
+           c: color(),
+           hist: [],
+           pr: p
+        };
     }
 }
 
 function add() {
-    var p = randomFromInterval(10, 40);
     N+=1;
-    cx.push(0);
-    cy.push(10);
-    pr.push(p);
-    x.push(randomPos(p));
-    y.push(randomPos(p));
-    hist.push([]);
-    c.push(color());
+    var p = randomFromInterval(10, 40);
+    objects.push({
+       x: randomPos(p),
+       y: randomPos(p),
+       c: color(),
+       hist: [],
+       pr: p,
+       cx: 1,
+       cy: 10
+    });
 }
 
 function showVector(i) {
+    var o = objects[i];
     ctx.beginPath();
-    ctx.moveTo(x[i],y[i]);
-    ctx.lineTo(x[i]+cx[i]*3,y[i]+cy[i]*3);
+    ctx.moveTo(o.x,o.y);
+    ctx.lineTo(o.x+o.cx*3,o.y+o.cy*3);
     ctx.lineWidth = 5;
-    ctx.sorkeStyle = c[i];
+    ctx.sorkeStyle = o.c;
     ctx.stroke();
 }
 
 function showLine(i) {
-    var h = hist[i];
+    var h = objects[i].hist;
     var length = h.length;
     if(length > 1) {
         ctx.beginPath();
         ctx.moveTo(h[0].x,h[0].y);
-        for (var j=1; j<length; j++) {
-            ctx.lineTo(h[j].x, h[j].y);
-        }
+        for (var j=1; j<length; j++) ctx.lineTo(h[j].x, h[j].y);
         ctx.lineWidth = 1;
-        ctx.strokeStyle = c[i];
+        ctx.strokeStyle = objects[i].c
         ctx.stroke();
     }
 }
 
 function showCircle(i) {
+    var o = objects[i];
     ctx.beginPath();
-    ctx.arc(x[i], y[i], pr[i], 0, 2 * Math.PI, false);
-    ctx.fillStyle = c[i];
+    ctx.arc(o.x, o.y, o.pr, 0, 2 * Math.PI, false);
+    ctx.fillStyle = o.c;
     ctx.fill();
 }
 
@@ -93,111 +92,101 @@ function display() {
     }
 }
 
-function roundSpeed() {
+function randomSpeed() {
     for(var i = 0; i<N; i++) {
-        cx[i] = 0;
-        cy[i] = 10;
+        objects[i].cx = 0;
+        objects[i].cy = 10;
     }
 }
 
 function rm() {
     if(N>0) {
-        cx.pop();
-        cy.pop();
-        x.pop();
-        y.pop()
-        c.pop();
-        pr.pop();
-        hist.pop();
+        objects.pop();
         N-=1;
     }
 }
 
 function run () {
     for (var i=0; i < N; i++){
+        var oi = objects[i];
 
-            x[i] += cx[i];
-            y[i] += cy[i];
+        oi.x += oi.cx;
+        oi.y += oi.cy;
 
-            if(line) hist[i].push({x: x[i], y:y[i]});
+        if(line) oi.hist.push({x: oi.x, y: oi.y});
 
-            if(x[i] <= pr[i]) {
-                cx[i]*=-1;
-            }
+        if(oi.x <= oi.pr) {
+            oi.cx*=-1;
+        }
 
-            if(x[i] >= size-pr[i]) {
-                cx[i]*=-1;
-            }
+        if(oi.x >= size-oi.pr) {
+            oi.cx*=-1;
+        }
 
-            if(y[i] <= pr[i]) {
-                cy[i]*=-1;
-            }
+        if(oi.y <= oi.pr) {
+            oi.cy*=-1;
+        }
 
-            if(y[i] >= size-pr[i]) {
-                cy[i]*=-1;
-            }
-            
-            for(var j=0; j<N; ++j) {
-                if(x[j] != -1 && y[j] != -1 && i != j) {
+        if(oi.y >= size-oi.pr) {
+            oi.cy*=-1;
+        }
+        
+        for(var j=0; j<N; ++j) {
+            var oj = objects[j];
+            if(i != j) {
 
-                    x1 = x[i];
-                    x2 = x[j];
+                x1 = oi.x;
+                x2 = oj.x;
 
-                    y1 = y[i];
-                    y2 = y[j];
+                y1 = oi.y;
+                y2 = oj.y;
 
-                    l =Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+                l =Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
 
-                    if(l <= pr[i]+pr[j]) {
+                if(l <= oi.pr+oj.pr) {
 
-                        v1.x = cx[i];
-                        v1.y = cy[i];
-                        v2.x = cx[j];
-                        v2.y = cy[j];
-                        v2 = {
-                            x: cx[j],
-                            y: cy[j]
-                        };
+                    v1.x = oi.cx;
+                    v1.y = oi.cy;
+                    v2.x = oj.cx;
+                    v2.y = oj.cy
 
-                        u = {
-                            1: pr[i]/(pr[i]+pr[j]),
-                            2: pr[j]/(pr[i]+pr[j])
-                        };
+                    u = {
+                        1: oi.pr/(oi.pr+oj.pr),
+                        2: oj.pr/(oi.pr+oj.pr)
+                    };
 
-                        n = {
-                            x: (x2-x1)/l,
-                            y: (y2-y1)/l
-                        };
+                    n = {
+                        x: (x2-x1)/l,
+                        y: (y2-y1)/l
+                    };
 
-                        t = {
-                            x: (-1*(y2-y1))/l,
-                            y: (x2-x1)/l
-                        };
+                    t = {
+                        x: (-1*(y2-y1))/l,
+                        y: (x2-x1)/l
+                    };
 
-                        v1 = {
-                            n: (v1.x*(x2-x1) + v1.y*(y2-y1))/l,
-                            t: (-1*v1.x*(y2-y1) + v1.y*(x2-x1))/l,
-                        };
+                    v1 = {
+                        n: (v1.x*(x2-x1) + v1.y*(y2-y1))/l,
+                        t: (-1*v1.x*(y2-y1) + v1.y*(x2-x1))/l,
+                    };
 
-                        v2 = {
-                            n: (v2.x*(x2-x1) + v2.y*(y2-y1))/l,
-                            t: (-1*v2.x*(y2-y1) + v2.y*(x2-x1))/l
-                        }
-
-                        v1.nP =  (u[1]-u[2])*v1.n + 2*u[2]*v2.n;
-                        v2.nP =  -1*(u[1]-u[2])*v2.n + 2*u[1]*v1.n;
-
-
-
-                        cx[i] = v1.nP*n.x + v1.t*t.x;
-                        cy[i] = v1.nP*n.y + v1.t*t.y;
-
-                        cx[j] = v2.nP*n.x + v2.t*t.x;
-                        cy[j] = v2.nP*n.y + v2.t*t.y;
-
+                    v2 = {
+                        n: (v2.x*(x2-x1) + v2.y*(y2-y1))/l,
+                        t: (-1*v2.x*(y2-y1) + v2.y*(x2-x1))/l
                     }
+
+                    v1.nP =  (u[1]-u[2])*v1.n + 2*u[2]*v2.n;
+                    v2.nP =  -1*(u[1]-u[2])*v2.n + 2*u[1]*v1.n;
+
+                    oi.cx = v1.nP*n.x + v1.t*t.x;
+                    oi.cy = v1.nP*n.y + v1.t*t.y;
+
+                    oj.cx = v2.nP*n.x + v2.t*t.x;
+                    oj.cy = v2.nP*n.y + v2.t*t.y;
+
                 }
-            }   
+            }
+        }
     }
 }
 
@@ -226,13 +215,13 @@ function showLineBtn () {
     if(line) {
         line = false;
         for(var i = 0; i<N; i++) {
-            hist[i] = [];
+            objects[i].hist = [];
         }
     }else line = true;
 }
 
 init();
-roundSpeed();
+randomSpeed();
 
 var t = setInterval(function(){
     step();
